ba891128b266fca822d9792b00a3ed32827a93f9
Moe <emsfoit@gmail.com>
2020-03-10 15:00:38 +0100

    add config to mailer to work in dev mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,6 @@ Rails.application.configure do
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
-    config.action_controller.enable_fragment_cache_logging = true
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
@@ -28,16 +27,19 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
-
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { :host => 'http://localhost:3000' }
+
+  config.action_mailer.delivery_method = :letter_opener
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
+  #config.active_job.queue_adapter     = :resque
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
@@ -52,8 +54,9 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+  config.action_mailer.asset_host = 'http://localhost:3000'
 
-  # Raises error for missing translations.
+  # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
   # Use an evented file watcher to asynchronously detect changes in source code,
